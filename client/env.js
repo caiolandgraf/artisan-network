29c867e6bcf04839eaf2ff8c35aa516e62d4394e
caio <eu.caiolandgraf@gmail.com>
2023-08-04 23:59:42 -0300

    first commit

---
+++ client/env.js
@@ -0,0 +1,4 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+export const REACT_APP_API_URL = process.env.REACT_APP_API_URL;
---
+++ client/src/theme.js
@@ -0,0 +1,128 @@
+// color design tokens export
+export const colorTokens = {
+    grey: {
+        0: "#FFFFFF",
+        10: "#F6F6F6",
+        50: "#F0F0F0",
+        100: "#E0E0E0",
+        200: "#C2C2C2",
+        300: "#A3A3A3",
+        400: "#858585",
+        500: "#666666",
+        600: "#4D4D4D",
+        700: "#333333",
+        800: "#1A1A1A",
+        900: "#0A0A0A",
+        1000: "#000000",
+    },
+    primary: {
+        50: "#E6FBFF",
+        100: "#CCF7FE",
+        200: "#99EEFD",
+        300: "#66E6FC",
+        400: "#33DDFB",
+        500: "#00D5FA",
+        600: "#00A0BC",
+        700: "#006B7D",
+        800: "#00353F",
+        900: "#001519",
+    },
+    red: {
+        50: "#fff5f5",
+        100: "#fed7d7",
+        200: "#feb2b2",
+        300: "#fc8181",
+        400: "#f56565",
+        500: "#fb4934",
+        600: "#e53e3e",
+        700: "#c53030",
+        800: "#9b2c2c",
+        900: "#742a2a",
+    },
+};
+
+// mui theme settings
+export const themeSettings = (mode) => {
+    return {
+        palette: {
+            mode: mode,
+            ...(mode === "dark"
+                ? {
+                    // palette values for dark mode
+                    primary: {
+                        dark: colorTokens.primary[200],
+                        main: colorTokens.primary[500],
+                        light: colorTokens.primary[800],
+                    },
+                    red: {
+                        dark: colorTokens.red[200],
+                        main: colorTokens.red[500],
+                        light: colorTokens.red[800],
+                    },
+                    neutral: {
+                        dark: colorTokens.grey[100],
+                        main: colorTokens.grey[200],
+                        mediumMain: colorTokens.grey[300],
+                        medium: colorTokens.grey[400],
+                        light: colorTokens.grey[700],
+                    },
+                    background: {
+                        default: colorTokens.grey[900],
+                        alt: colorTokens.grey[800],
+                    },
+                }
+                : {
+                    // palette values for light mode
+                    primary: {
+                        dark: colorTokens.primary[700],
+                        main: colorTokens.primary[500],
+                        light: colorTokens.primary[50],
+                    },
+                    red: {
+                        dark: colorTokens.red[700],
+                        main: colorTokens.red[500],
+                        light: colorTokens.red[50],
+                    },
+                    neutral: {
+                        dark: colorTokens.grey[700],
+                        main: colorTokens.grey[500],
+                        mediumMain: colorTokens.grey[400],
+                        medium: colorTokens.grey[300],
+                        light: colorTokens.grey[50],
+                    },
+                    background: {
+                        default: colorTokens.grey[10],
+                        alt: colorTokens.grey[0],
+                    },
+                }),
+        },
+        typography: {
+            fontFamily: ["Rubik", "sans-serif"].join(","),
+            fontSize: 12,
+            h1: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 40,
+            },
+            h2: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 32,
+            },
+            h3: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 24,
+            },
+            h4: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 20,
+            },
+            h5: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 16,
+            },
+            h6: {
+                fontFamily: ["Rubik", "sans-serif"].join(","),
+                fontSize: 14,
+            },
+        },
+    };
+};
