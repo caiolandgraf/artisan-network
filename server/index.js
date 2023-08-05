7bdbc499efbc7310f0e40315b3712deaff0b60a5
caio <eu.caiolandgraf@gmail.com>
2023-08-05 00:43:40 -0300

    Update index.js

--- server/index.js
+++ server/index.js
@@ -51,6 +51,10 @@ const storage = multer.diskStorage({
 
 const upload = multer({ storage })
 
+app.get('/', (req, res) => {
+    res.send('Express JS on Vercel')
+})
+
 /* Routes with files */
 app.post("/auth/register", upload.single("picture"), register)
 app.post("/posts", verifyToken, upload.single("picture"), createPost)
